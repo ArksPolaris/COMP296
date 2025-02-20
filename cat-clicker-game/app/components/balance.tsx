ce45527144e73a64f5ddf969570ba616c446607e
Monk <129611538+ArksPolaris@users.noreply.github.com>
2025-02-19 19:34:43 -0500

    moved the donation function to the balance file

--- cat-clicker-game/app/components/balance.tsx
+++ cat-clicker-game/app/components/balance.tsx
@@ -9,6 +9,17 @@ export function updateDisplay() {
     }
 }
 
+export function donation() {
+    var random = Math.floor(Math.random() * 100);
+    if (random > 45) {
+        alert("An adopter came by and gave you a donation.");
+        addfunds(45);
+    } else {
+        alert("Seems like no one came by to adopt a cat, but one of the locals gave a small donation.");
+        addfunds(10);
+    }
+}
+
 export function addfunds(val: number) {
     balance+=val;
     updateDisplay();
